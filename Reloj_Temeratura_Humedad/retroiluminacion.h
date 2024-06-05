91f65b647dfa5846e8ae6c19d036e371c96cb45d
JMTS <74095359+JVRMTS@users.noreply.github.com>
2024-06-05 09:16:29 +0200

    Configuración de la retroiluminacion para la versión 3.0.0 de ESP32

--- Reloj_Temeratura_Humedad/retroiluminacion.h
+++ Reloj_Temeratura_Humedad/retroiluminacion.h
@@ -1,32 +1,32 @@
 /* 
  * Control de la retroiluminación de la pantalla, mediante la lectura de una LDR
- * Escribimos en PWM con la libreria PWMOutESP32
+ * Con la version 3.0.0 de Esp32 podemos utilizar analogWrite en con el Esp32, se a configurar ledcAttach,
+ * si no se configura la pantalla parpadeará dependiendo de la iluminación.
  * @JMTS - www.jmts.es
- * Creado el 28/04/2024
+ * Creado el 05/06/2024
  */
 
 // Asignamos los pines de la iluminación del LCD y del LDR y declaramos sus variables
 #define PINLCD 18
 #define PINLDR 32
-
 // Definimos el minimo de retroiluminación
-#define MINIMO 50
-
+#define MINIMO 16
 unsigned int valorLDR;
 unsigned int valorLCD;
 
-
-#include <PWMOutESP32.h>
-PWMOutESP32 pwm(12,5000); // Ajustamos la resolución de 12 bits y una frecuencia de 5000Hz
+// Configuración canal analógico
+void configLedc(){
+  ledcAttach(PINLCD, 5000, 12);
+}
 
 void iluminacion(){
 // Comprobamos la intensidad de la luz
-  valorLDR = analogRead(PINLDR); // Leemos la variable del LDR
+  valorLDR = analogRead(PINLDR)/4; // Leemos la variable del LDR
 // Comprobamos una intenisad lumínica mínima para que no se apague nunca el LCD
   if(valorLDR <= MINIMO){
     valorLCD = MINIMO;        // Ajustamos un mínimo de iluminación
   }else{
     valorLCD = valorLDR;  // Asignamos el valor de iluminación del LCD en base al valor del LDR
   }
-  pwm.analogWrite(PINLCD, valorLCD); // Escribimos en el pinLCD el valor de la retroiluminación
+  analogWrite(PINLCD, valorLCD); // Escribimos en el pinLCD el valor de la retroiluminación
 }
